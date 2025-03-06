9fe818a08aab241e023226743a999027c082585b
cdomsal <cristianmdominguezsalas.gmail.com>
2025-03-06 19:13:54 +0100

    POO - Videojuegos

---
+++ POO/prueba.py
@@ -0,0 +1,13 @@
+from videojuegos import Videojuego
+
+resident_evil = Videojuego(
+    "Resident Evil",
+    ["Terror", "Acción"],
+    "5/5/1996",
+    9.6,
+    18,
+    10.99,
+    1.45
+)
+
+print(resident_evil)
