94237e0ab7a81d0776565f1b997c448d6135d9fe
cdomsal <cristianmdominguezsalas.gmail.com>
2025-03-06 20:16:37 +0100

    Juegos añadidos

--- POO/Ejercicio1.py
+++ POO/Ejercicio1.py
@@ -7,5 +7,11 @@ lista_juegos = [
     Videojuego("Minecraft", ["Supervivencia", "Sandbox"], "2011-11-18", 9.0, 7, 26.95, 1.2),
     Videojuego("Red Dead Redemption", ["ACCION", "MUNDO ABIERTO"], "2018-10-26", 9.8, 18, 59.99, 150),
     Videojuego("God of War(2018)", ["ACCION", "AVENTURA"], "2018-04-20", 9.6, 18, 49.99, 45),
-    Videojuego("Dark Souls III", ["RPG", "ACCION"], "2016-04-12", 9.3, 16, 39.99, 25)
+    Videojuego("Dark Souls III", ["RPG", "ACCION"], "2016-04-12", 9.3, 16, 39.99, 25),
+    Videojuego("Animal Crossing: New Horizons", ["SIMULACIÓN", "SOCIAL"], "2020-03-20", 9.1, 3, 49.99, 10.2),
+    Videojuego("Cyberpunk 2077", ["RPG", "MUNDO ABIERTO"], "2020-12-10", 8.5, 18, 59.99, 70),
+    Videojuego("Super Mario Odyssey", ["PLATAFORMAS", "AVENTURA"], "2017-10-27", 9.8, 7, 49.99, 5.6)
 ]
+
+for v in lista_juegos:
+    print(v)
